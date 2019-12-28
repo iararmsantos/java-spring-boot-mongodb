69a540822dda82da0d76997c8078af0f0598b3c6
Iara Santos <iararmsantos@gmail.com>
2019-12-28 11:50:11 -0500

    Endpoint to return posts from a user

--- src/main/java/com/iarasantos/mongodb/resources/UserResource.java
+++ src/main/java/com/iarasantos/mongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.iarasantos.mongodb.domain.Post;
 import com.iarasantos.mongodb.domain.User;
 import com.iarasantos.mongodb.dto.UserDTO;
 import com.iarasantos.mongodb.services.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
