8be8147a22c1996e25836ba1e8c8e3975745e019
Iara Santos <iararmsantos@gmail.com>
2019-12-28 12:07:29 -0500

    Find post by id

---
+++ src/main/java/com/iarasantos/mongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.iarasantos.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iarasantos.mongodb.domain.Post;
+import com.iarasantos.mongodb.repository.PostRepository;
+import com.iarasantos.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));		
+	}	
+}
